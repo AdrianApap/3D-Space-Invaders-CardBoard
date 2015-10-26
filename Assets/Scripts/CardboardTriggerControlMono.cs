aa57b2a7bc0c79799168e38f6b6e7af3a64461e6
Adrian <apapadrian@gmail.com>
2015-10-26 21:13:18 +0100

    Added click script with spawn cube to debug

---
+++ Assets/Scripts/CardboardTriggerControlMono.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardTriggerControlMono : MonoBehaviour
+{
+    public bool magnetDetectionEnabled = true;
+    public GameObject Cube;
+
+    void Start()
+    {
+        CardboardMagnetSensor.SetEnabled(magnetDetectionEnabled);
+        // Disable screen dimming:
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    void Update() {
+        if (!magnetDetectionEnabled) return;
+        if (CardboardMagnetSensor.CheckIfWasClicked())
+        {
+            Debug.Log("DO SOMETHING HERE");  // PERFORM ACTION.
+            Instantiate(Cube, transform.position, Quaternion.identity);
+            CardboardMagnetSensor.ResetClick();
+        }
+    }
+}
