7d185bb757e23f76da6ec214380a9d7bdd2cd37d
Adrian <apapadrian@gmail.com>
2015-10-27 21:17:12 +0100

    Added Shooting Script (Shoots Spheres, Need to fix angles)

---
+++ Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootLaser : MonoBehaviour {
+    public Rigidbody projectile;
+    public float speed = 20;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("space"))
+        {
+            print("Shooting");
+            this.shoot();
+        }
+    }
+
+    void shoot()
+    {
+        Rigidbody myBullet = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+        myBullet.velocity = transform.TransformDirection(new Vector3(Camera.main.transform.eulerAngles.y, 360-Camera.main.transform.eulerAngles.x, speed));
+    }
+}
