ed94a39e6df1364d35355f62600283e00a89c5e9
Juan Buhagiar <juan.buhagiar@iaeste.org.mt>
2015-10-31 21:06:08 +0100

    Moving Invaders

--- Assets/Scripts/SpawnInvaders.cs
+++ Assets/Scripts/SpawnInvaders.cs
@@ -4,15 +4,21 @@ using System.Collections;
 public class SpawnInvaders : MonoBehaviour {
     public static int invaderCount = 20;
     public GameObject invader;
+    private GameObject[] invaders;
 
     // Use this for initialization
     void Start () {
+        invaders = new GameObject[invaderCount];
         this.spawnInvaders();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        for (int i = 0; i < invaderCount; i++)
+        {
+            Vector3 pos = invaders[i].transform.position;
+            invaders[i].transform.Translate(Vector3.right * Time.deltaTime, Camera.main.transform);
+        }
 	}
 
     void spawnInvaders()
@@ -20,7 +26,7 @@ public class SpawnInvaders : MonoBehaviour {
         for (int i = 0; i < invaderCount; i++)
         {
             Vector3 pos = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50));
-            GameObject myInvader = Instantiate(invader, pos, transform.rotation) as GameObject;
+            invaders[i] = Instantiate(invader, pos, transform.rotation) as GameObject;
         }
     }
 }
