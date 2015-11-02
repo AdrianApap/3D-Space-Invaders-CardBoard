b0e4e411ecf441ee5fa63496bbcdcddbee5cdb34
Juan Buhagiar <juan.buhagiar@iaeste.org.mt>
2015-11-02 20:39:54 +0100

    added health bar

---
+++ Assets/Scripts/healthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthBar : MonoBehaviour
+{
+    public Texture tex;
+    int lives = 3;
+
+    private float texWidth;
+    private float texHeight;
+ 
+ void Start()
+    {
+        texWidth = 10;
+        texHeight = 30;
+    }
+
+    void OnGUI()
+    {
+        if (lives > 0)
+        {
+            for (int i = 0; i < lives; i++)
+            {
+                Rect posRect = new Rect(20+(i*30), 20, texWidth * lives, texHeight);
+                GUI.DrawTexture(posRect, tex);
+            }
+        }
+    }
+}
